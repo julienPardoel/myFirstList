d63b980a7551ffb9890503f8f114a76763f40fd7
julienPardoel <julien.pardoel@gmail.com>
2021-02-08 15:54:53 +0100

    fin de journée

---
+++ main.js
@@ -0,0 +1,36 @@
+// button creation
+
+const addButton = document.querySelector('#logInBtn');
+const userList = document.querySelector('#userList');
+const logInPseudo = document.querySelector('#logInPseudo');
+const logInEmail = document.querySelector('#logInEmail');
+
+const li = document.createElement('li');
+
+li.appendChild(document.createTextNode(`${logInPseudo.value} : ${logInEmail.value}`));
+
+addButton.addEventListener('click',(event) =>{    
+    event.preventDefault();
+    const li = document.createElement('li');
+    li.className = "user";    
+    li.appendChild(document.createTextNode(`${logInPseudo.value} : ${logInEmail.value}`));   
+    if(userList.appendChild(li)){    
+        logInPseudo.value = '';
+        logInEmail.value = '';
+    }
+});
+
+// button suppression
+
+const supprButton = document.querySelector('#supprBtn');
+
+
+supprButton.addEventListener('click',(event) =>{
+    // event.preventDefault();
+    // const li = document.removeElement('li');
+    // ul.removeChild(ul);
+    console.log('supprimer');
+    // userList.classList.toggle('hiddenList');
+    userList.remove(li)
+});
+
