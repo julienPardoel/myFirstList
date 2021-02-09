e10db77494bf3b5a8100528fc1b93505ea2542db
julienPardoel <julien.pardoel@gmail.com>
2021-02-09 10:05:10 +0100

    nettoyage code

--- main.js
+++ main.js
@@ -3,20 +3,20 @@
 const addButton = document.querySelector('#logInBtn');
 const userList = document.querySelector('#userList');
 const logInPseudo = document.querySelector('#logInPseudo');
-const logInEmail = document.querySelector('#logInEmail');
+// const logInEmail = document.querySelector('#logInEmail');
 
 const li = document.createElement('li');
 
-li.appendChild(document.createTextNode(`${logInPseudo.value} : ${logInEmail.value}`));
+li.appendChild(document.createTextNode(`${logInPseudo.value}`));
 
 addButton.addEventListener('click',(event) =>{    
     event.preventDefault();
-    const li = document.createElement('li');
+    // const li = document.createElement('li');
     li.className = "user";    
-    li.appendChild(document.createTextNode(`${logInPseudo.value} : ${logInEmail.value}`));   
+    li.appendChild(document.createTextNode(`${logInPseudo.value}`));   
     if(userList.appendChild(li)){    
         logInPseudo.value = '';
-        logInEmail.value = '';
+        // logInEmail.value = '';
     }
 });
 
