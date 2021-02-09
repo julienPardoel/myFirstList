b1e655357a790b555838a9b5404a534b9e5e3caf
julienPardoel <julien.pardoel@gmail.com>
2021-02-09 13:14:10 +0100

    nettoyage code

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
-// button creation
-
 const addButton = document.querySelector('#logInBtn');
 const userList = document.querySelector('#userList');
 const logInPseudo = document.querySelector('#logInPseudo');
-// const logInEmail = document.querySelector('#logInEmail');
-
 const li = document.createElement('li');
 
 li.appendChild(document.createTextNode(`${logInPseudo.value}`));
 
-addButton.addEventListener('click',(event) =>{    
-    event.preventDefault();
-    // const li = document.createElement('li');
-    li.className = "user";    
-    li.appendChild(document.createTextNode(`${logInPseudo.value}`));   
-    if(userList.appendChild(li)){    
-        logInPseudo.value = '';
-        // logInEmail.value = '';
+addButton.addEventListener('click',(event) =>{ // Au click sur le bouton créer ...
+
+    const li = document.createElement('li'); // ... crée un element li
+
+    li.className = "user"; // ... donne une class à li
+
+    li.appendChild(document.createTextNode(`${logInPseudo.value}`)); // ... ?
+
+    if(logInPseudo.value !== ""){ // ... si mon input est non vide ...
+
+        logInPseudo.value = ''; // ... ça vide mon input
+
+        const checkBtn = document.createElement('button'); // ... crée un bouton check
+        const supprBtn = document.createElement('button'); // ... crée un bouton suppr
+
+        checkBtn.innerHTML = '<i class="far fa-check-circle"></i>'; // ... donne du html à check
+        supprBtn.innerHTML = '<i class="far fa-trash-alt"></i>'; // ... donne du html à supor
+
+        checkBtn.className = "checkBtn"; // ... donne une class à l'élement
+        supprBtn.className = "supprBtn"; // ... donne une class à l'élement
+        
+        li.appendChild(checkBtn); // ... 
+        li.appendChild(supprBtn); // ...
+
+        userList.appendChild(li); // ...
+
     }
+
 });
 
-// button suppression
+userList.addEventListener('click',(event) => { // au click sur un élement de li...
 
-const supprButton = document.querySelector('#supprBtn');
+    console.log(event.target.parentNode); // ... afficher dans la console le parent de la target
+
+    const button = event.target.parentNode; // ... création d'une variable contenant le parent de la target 
+
+    if(button.classList.contains('supprBtn')){ // ... si la variable button contient la class supprBtn ...
+
+        button.parentNode.remove(); // ...le parent du button est supprimé
+
+    };
+
+    if(button.classList.contains('checkBtn')){ //... si la variable button contient la class checkBtn
+
+        button.parentNode.style.color = 'green'; // ... la couleur du text du parent devient vert
+
+    }else if(button.parentNode.style.color = 'green'){ // ... si la couleur du text du parent est vert
+
+        button.parentNode.style.color = 'white'; // ... la couleur du text du parent devient blanche
+
+    };
 
 
-supprButton.addEventListener('click',(event) =>{
-    console.log('supprimer');
-    userList.remove(li)
+
 });
 
-// rajout pour test github commit
+
+
+
+
